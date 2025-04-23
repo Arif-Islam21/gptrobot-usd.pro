3d133f9964df041b772454a1579cc23eaeca0ced
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-04-23 11:29:32 +0600

    icons added

--- src/component/extra/home/PriceList.jsx
+++ src/component/extra/home/PriceList.jsx
@@ -1,71 +1,82 @@
 import { Table } from "react-bootstrap";
+import Binance from "../../../assets/images/coins/Binance.jpg";
+import Bitcoin from "../../../assets/images/coins/Bitcoin.png";
+import Cardano from "../../../assets/images/coins/Cardano.png";
+import Dogecoin from "../../../assets/images/coins/Dogecoin.jpg";
+import Ethereum from "../../../assets/images/coins/Ethereum.jpg";
+import Litecoin from "../../../assets/images/coins/Litecoin.jpg";
+import Polkadot from "../../../assets/images/coins/Polkadot.png";
+import Ripple from "../../../assets/images/coins/Ripple.jpg";
+import Shiba from "../../../assets/images/coins/Shiba.png";
+import Solana from "../../../assets/images/coins/Solana.jpg";
+import TRON from "../../../assets/images/coins/TRON.png";
 
 const PriceList = () => {
   const prices = [
     {
       name: "Bitcoin",
       symbol: "BTC",
-      logo: "Bitcoin Logo",
+      logo: Bitcoin,
       price: "$91,510",
     },
     {
       name: "TRON",
       symbol: "TRX",
-      logo: "TRON Logo",
+      logo: TRON,
       price: "$0.247",
     },
     {
       name: "Solana",
       symbol: "SOL",
-      logo: "Solana Logo",
+      logo: Solana,
       price: "$145.02",
     },
     {
       name: "Cardano",
       symbol: "ADA",
-      logo: "Cardano Logo",
+      logo: Cardano,
       price: "$0.658",
     },
     {
       name: "Litecoin",
       symbol: "LTC",
-      logo: "Litecoin Logo",
+      logo: Litecoin,
       price: "$83.32",
     },
     {
       name: "Ethereum",
       symbol: "ETH",
-      logo: "Ethereum Logo",
+      logo: Ethereum,
       price: "$1,702.96",
     },
     {
       name: "Ripple",
       symbol: "XRP",
-      logo: "Ripple Logo",
+      logo: Ripple,
       price: "$2.16",
     },
     {
       name: "Polkadot",
       symbol: "DOT",
-      logo: "Polkadot Logo",
+      logo: Polkadot,
       price: "$3.91",
     },
     {
       name: "Dogecoin",
       symbol: "DOGE",
-      logo: "Dogecoin Logo",
+      logo: Dogecoin,
       price: "$0.172",
     },
     {
       name: "Binance Coin",
       symbol: "BNB",
-      logo: "Binance Coin Logo",
+      logo: Binance,
       price: "$608.91",
     },
     {
       name: "Shiba",
       symbol: "SHIB",
-      logo: "Shiba Inu Logo",
+      logo: Shiba,
       price: "$0",
     },
   ];
@@ -85,7 +96,14 @@ const PriceList = () => {
           {prices.map((price, index) => (
             <tr key={index}>
               <td className="text-center">{price.name}</td>
-              <td className="text-center">{price.logo}</td>
+              <td className="text-center">
+                <img
+                  className=""
+                  style={{ height: "5vh", width: "5vh" }}
+                  src={price.logo}
+                  alt={price.logo}
+                />
+              </td>
               <td className="text-center">{price.price}</td>
             </tr>
           ))}
